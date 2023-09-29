58adb6132ba753f8dae0bc67c9736260c0a2dc08
olivia474 <olivia.lengthorn@bp.com>
2023-09-29 16:24:57 +0100

    Added a simple multiplier

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿namespace Calculator;
+﻿using System.ComponentModel;
+
+namespace Calculator;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the calculator!");
+        
+        Console.Write("Enter first radius: ");
+        double radius1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Enter second radius: ");
+        double radius2 = Convert.ToDouble(Console.ReadLine());
+        
+        double AreaOfACircle = Math.PI * radius1 * radius2;
+        Console.WriteLine("Area of a Circle = " + AreaOfACircle);
+
+        Console.ReadLine();
+        
+
     }
 }
