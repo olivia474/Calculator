f2cae15b15d40b06e47c94f9e8762393f854cd2d
olivia474 <olivia.lengthorn@bp.com>
2023-10-10 09:51:24 +0100

    Code works

--- Program.cs
+++ Program.cs
@@ -4,53 +4,54 @@ using System.Data.Common;
 namespace Calculator;
 class Program
 {
-
-    static void PrintWelcomeMessage() {
-
-        Console.WriteLine("Welcome to the calculator!");
-        Console.WriteLine("=========================="); }
-
-    static void PerformOneCalculation() {
-
-        Console.Write("Please enter the operator: +, -, *, /: ");
-        string? Operator = Console.ReadLine();
-        
-        Console.Write("Please enter the first number: ");
-        double firstNumber = Convert.ToDouble(Console.ReadLine());
-        
-        Console.Write("Please enter the second number: ");
-        double secondNumber = Convert.ToDouble(Console.ReadLine()); 
-    
-        if(Operator == "+") {
-            double equation = firstNumber + secondNumber;
-            Console.WriteLine("The answer is: " + equation);
-        }
-
-        else if(Operator == "-") {
-            double equation = firstNumber - secondNumber;
-            Console.WriteLine("The answer is: " + equation);
-        }
-
-        else if(Operator == "*"){
-            double equation = firstNumber * secondNumber;
-            Console.WriteLine("The answer is: " + equation);
-        }
-
-        else if(Operator == "/") {
-            double equation = firstNumber / secondNumber;
-            Console.WriteLine("The answer is: " + equation);
-        }
-
-        else {
-            Console.WriteLine("Error");
-        }
-
-        Console.ReadLine();
-    }
     static void Main(string[] args)
     {
         PrintWelcomeMessage();
         PerformOneCalculation();
-    
+
     }
+    static void PrintWelcomeMessage() {
+        
+        Console.WriteLine("Welcome to the calculator!");
+        Console.WriteLine("=========================="); 
+        }
+    static void PerformOneCalculation() {
+
+        Console.Write("Please enter the operator: +, -, *, /: ");
+        string? Operator = Console.ReadLine(); 
+
+        Console.Write("How many numbers do you want to " + Operator + ":");
+        int numbers = Convert.ToInt32(Console.ReadLine());
+
+        int result = 0;
+       
+        for(int i= 0; i < numbers; i++) {
+            Console.Write("Please enter number " + (i+1) + ":");
+            int number = Convert.ToInt32(Console.ReadLine());
+        
+        if(Operator == "+") {
+            result+=number;
+        }
+
+        else if(Operator == "-") {
+            result-=number;
+        }
+
+        else if(Operator == "*") {
+            result*=number;
+        }
+
+        else if(Operator == "/") {
+            result/=number;
+        }
+        
+        else {
+            Console.WriteLine("Error");
+        }
+       }
+
+        Console.WriteLine("The result is: " + result);
+        
+        Console.ReadLine();
+   }
 }
